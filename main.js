91b084c649b275207b72dde9e8e1c0a924e5e720
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-09-28 22:55:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//get modal element 
+var modal = document.getElementById('simpleModal');
+
+//get open modal button
+var modalBtn = document.getElementById('modalBtn');
+
+//get close button
+var closeBtn = document.getElementById('closeBtn');
+
+//listen for open click
+modalBtn.addEventListener('click', openModal);
+
+//listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+//function to open modal
+function open() {}
+  modal.style.display = 'block';
+}
+
+//function to close modal
+function open() {}
+  modal.style.display = 'none';
+}
